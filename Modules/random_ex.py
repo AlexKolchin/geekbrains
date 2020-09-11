22034853d0225d348e1e41c5ebb19eb875642413
okolc <akakolchin@gmail.com>
2020-09-11 18:06:21 +0300

    standart modules random and math

---
+++ Modules/random_ex.py
@@ -0,0 +1,15 @@
+from random import randint, choice, sample, shuffle
+
+#1. загадать случайное число от 0 до 100
+print(randint(1, 100))
+#2. выбрать победителя лотереи из списка players
+players = ['Max', 'Leo', 'Kate', 'Ron', 'Bill']
+print(choice(players))
+#3. выбрать 3-х победителей лотерей из списка players
+
+print(sample(players, 3))
+#4. перемешать карты в стопке (списке) cards
+cards = ['6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+print(cards)
+shuffle(cards)
+print(cards)
