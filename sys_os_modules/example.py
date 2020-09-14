a751395cc701d68b6b2a2159772141dcf7a938d8
okolc <akakolchin@gmail.com>
2020-09-14 15:44:21 +0300

    update sys and os

---
+++ sys_os_modules/example.py
@@ -0,0 +1,27 @@
+'''
+В зависимости от параметр вызывать разичные функции скрипта
+Параметр ping -> функция выводит pong
+2 параметра name <Имя> -> функция приветсвия пользователя
+параметр list показать содержимое текущей директории
+'''
+
+import sys, os
+
+def ping():
+    print('pong')
+
+def hello(name):
+    print('Hello', name)
+
+def get_info():
+    print(os.listdir())
+
+command = sys.argv[1]
+
+if command == 'ping':
+    ping()
+elif command == 'list':
+    get_info()
+elif command == 'name':
+    name = sys.argv[2]
+    hello(name)
